b13fa8a94ee502c5521e0708852d97b1e04f22d0
Dominik <dominik.weissenseel@gmx.de>
2018-04-03 23:58:14 +0200

    Changed color from RGBA to ARGB

--- src/main/java/de/domisum/lib/snaporta/Snaporta.java
+++ src/main/java/de/domisum/lib/snaporta/Snaporta.java
@@ -1,34 +1,43 @@
 package de.domisum.lib.snaporta;
 
+import de.domisum.lib.auxilium.util.java.annotations.API;
+import de.domisum.lib.snaporta.color.SnaportaColor;
 import de.domisum.lib.snaporta.util.ARGBUtil;
 
 public interface Snaporta
 {
 
-	int getWidth();
+	@API int getWidth();
 
-	int getHeight();
-
-
-	int getARGBAt(int x, int y);
+	@API int getHeight();
 
 
-	default int getAlphaAt(int x, int y)
+	@API int getARGBAt(int x, int y);
+
+
+	@API default SnaportaColor getColorAt(int x, int y)
+	{
+		int argb = getARGBAt(x, y);
+		return SnaportaColor.fromARGBInt(argb);
+	}
+
+
+	@API default int getAlphaAt(int x, int y)
 	{
 		return ARGBUtil.getAlphaComponent(getARGBAt(x, y));
 	}
 
-	default int getRedAt(int x, int y)
+	@API default int getRedAt(int x, int y)
 	{
 		return ARGBUtil.getRedComponent(getARGBAt(x, y));
 	}
 
-	default int getGreenAt(int x, int y)
+	@API default int getGreenAt(int x, int y)
 	{
 		return ARGBUtil.getGreenComponent(getARGBAt(x, y));
 	}
 
-	default int getBlueAt(int x, int y)
+	@API default int getBlueAt(int x, int y)
 	{
 		return ARGBUtil.getBlueComponent(getARGBAt(x, y));
 	}
--- src/main/java/de/domisum/lib/snaporta/color/SnaportaColor.java
+++ src/main/java/de/domisum/lib/snaporta/color/SnaportaColor.java
@@ -1,6 +1,7 @@
 package de.domisum.lib.snaporta.color;
 
 import de.domisum.lib.auxilium.util.java.annotations.API;
+import de.domisum.lib.snaporta.util.ARGBUtil;
 import de.domisum.lib.snaporta.util.SnaportaValidate;
 import lombok.Getter;
 
@@ -14,44 +15,54 @@ public final class SnaportaColor
 	@API public static final int ALPHA_OPAQUE = 255;
 
 	// ATTRIBUTES
+	@Getter private final int alpha;
 	@Getter private final int red;
 	@Getter private final int green;
 	@Getter private final int blue;
-	@Getter private final int alpha;
 
 
 	// INIT
-	private SnaportaColor(int red, int green, int blue, int alpha)
+	private SnaportaColor(int alpha, int red, int green, int blue)
 	{
+		SnaportaValidate.validateColorComponentInRange(alpha, "alpha");
 		SnaportaValidate.validateColorComponentInRange(red, "red");
 		SnaportaValidate.validateColorComponentInRange(green, "green");
 		SnaportaValidate.validateColorComponentInRange(blue, "blue");
-		SnaportaValidate.validateColorComponentInRange(alpha, "alpha");
 
+		this.alpha = alpha;
 		this.red = red;
 		this.green = green;
 		this.blue = blue;
-		this.alpha = alpha;
 	}
 
-	@API public static SnaportaColor fromRGBA(int red, int green, int blue, int alpha)
+	@API public static SnaportaColor fromARGBInt(int argb)
 	{
-		return new SnaportaColor(red, green, blue, alpha);
+		return fromARGB(
+				ARGBUtil.getAlphaComponent(argb),
+				ARGBUtil.getRedComponent(argb),
+				ARGBUtil.getBlueComponent(argb),
+				ARGBUtil.getBlueComponent(argb)
+		);
+	}
+
+	@API public static SnaportaColor fromARGB(int alpha, int red, int green, int blue)
+	{
+		return new SnaportaColor(alpha, red, green, blue);
 	}
 
 	@API public static SnaportaColor fromRGB(int red, int green, int blue)
 	{
-		return new SnaportaColor(red, green, blue, ALPHA_OPAQUE);
+		return new SnaportaColor(ALPHA_OPAQUE, red, green, blue);
 	}
 
 	@API public static SnaportaColor fromAwt(Color color)
 	{
-		return new SnaportaColor(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha());
+		return new SnaportaColor(color.getAlpha(), color.getRed(), color.getGreen(), color.getBlue());
 	}
 
 	@API public static SnaportaColor fullyTransparent()
 	{
-		return new SnaportaColor(0, 0, 0, ALPHA_TRANSPARENT);
+		return new SnaportaColor(ALPHA_TRANSPARENT, 0, 0, 0);
 	}
 
 
