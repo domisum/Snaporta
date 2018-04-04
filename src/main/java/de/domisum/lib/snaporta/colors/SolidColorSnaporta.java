43005160984ca2467141aa85aceac10334471297
Dominik <dominik.weissenseel@gmx.de>
2018-04-04 15:58:13 +0200

    Added solid color snaporta

---
+++ src/main/java/de/domisum/lib/snaporta/colors/SolidColorSnaporta.java
@@ -0,0 +1,29 @@
+package de.domisum.lib.snaporta.colors;
+
+import de.domisum.lib.auxilium.util.java.annotations.API;
+import de.domisum.lib.snaporta.Snaporta;
+import de.domisum.lib.snaporta.color.SnaportaColor;
+import de.domisum.lib.snaporta.util.SnaportaValidate;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@API
+@RequiredArgsConstructor
+public class SolidColorSnaporta implements Snaporta
+{
+
+	// ATTRIBUTES
+	@Getter private final int width;
+	@Getter private final int height;
+
+	@Getter private final SnaportaColor color;
+
+
+	// SNAPORTA
+	@Override public int getARGBAt(int x, int y)
+	{
+		SnaportaValidate.validateInBounds(this, x, y);
+		return color.toARGBInt();
+	}
+
+}
