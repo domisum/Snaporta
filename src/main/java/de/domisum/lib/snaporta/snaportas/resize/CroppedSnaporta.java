c9e930bfc4ab30da398f3014f5d6b2dd54275c5e
Dominik <dominik.weissenseel@gmx.de>
2018-04-05 00:57:51 +0200

    Added snaporta cropping

---
+++ src/main/java/de/domisum/lib/snaporta/snaportas/resize/CroppedSnaporta.java
@@ -0,0 +1,43 @@
+package de.domisum.lib.snaporta.snaportas.resize;
+
+import de.domisum.lib.auxilium.util.java.annotations.API;
+import de.domisum.lib.snaporta.Snaporta;
+import de.domisum.lib.snaporta.util.SnaportaValidate;
+import lombok.Getter;
+import org.apache.commons.lang3.Validate;
+
+@API
+public class CroppedSnaporta implements Snaporta
+{
+
+	private final Snaporta backingSnaporta;
+
+	private final int cropX;
+	private final int cropY;
+	@Getter private final int width;
+	@Getter private final int height;
+
+
+	// INIT
+	@API public CroppedSnaporta(Snaporta backingSnaporta, int cropX, int cropY, int width, int height)
+	{
+		Validate.isTrue(width >= 1, "width has to be at least 1, was "+width);
+		Validate.isTrue(height >= 1, "height has to be at least 1, was "+height);
+		SnaportaValidate.validateInInterval(0, width-1, "cropX+width", cropX+width);
+		SnaportaValidate.validateInInterval(0, height-1, "cropY+height", cropY+height);
+
+		this.backingSnaporta = backingSnaporta;
+		this.cropX = cropX;
+		this.cropY = cropY;
+		this.width = width;
+		this.height = height;
+	}
+
+
+	@Override public int getARGBAt(int x, int y)
+	{
+		SnaportaValidate.validateInBounds(this, x, y);
+		return backingSnaporta.getARGBAt(x-cropX, y-cropY);
+	}
+
+}
