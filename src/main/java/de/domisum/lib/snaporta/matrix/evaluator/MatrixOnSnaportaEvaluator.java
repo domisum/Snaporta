0987fb88a8887e9d591cc90006539fd589c682bf
Dominik <dominik.weissenseel@gmx.de>
2018-04-04 20:16:12 +0200

    Added matrix on snaporta evaluator

---
+++ src/main/java/de/domisum/lib/snaporta/matrix/evaluator/MatrixOnSnaportaEvaluator.java
@@ -0,0 +1,18 @@
+package de.domisum.lib.snaporta.matrix.evaluator;
+
+import de.domisum.lib.auxilium.util.java.annotations.API;
+import de.domisum.lib.snaporta.Snaporta;
+import de.domisum.lib.snaporta.color.Color;
+import de.domisum.lib.snaporta.matrix.Matrix;
+
+public interface MatrixOnSnaportaEvaluator
+{
+
+	int evaluateToARGB(Snaporta snaporta, Matrix matrix, int x, int y);
+
+	@API default Color evaluate(Snaporta snaporta, Matrix matrix, int x, int y)
+	{
+		return Color.fromARGBInt(evaluateToARGB(snaporta, matrix, x, y));
+	}
+
+}
