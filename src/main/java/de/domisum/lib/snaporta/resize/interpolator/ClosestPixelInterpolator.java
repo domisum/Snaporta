5df35e581f8ab20428f2c858a11f45d1196e76ea
Dominik <dominik.weissenseel@gmx.de>
2018-04-04 19:02:35 +0200

    Added resizing

---
+++ src/main/java/de/domisum/lib/snaporta/resize/interpolator/ClosestPixelInterpolator.java
@@ -0,0 +1,16 @@
+package de.domisum.lib.snaporta.resize.interpolator;
+
+import de.domisum.lib.snaporta.Snaporta;
+
+public class ClosestPixelInterpolator implements Interpolator
+{
+
+	@Override public int interpolateARGBAt(Snaporta snaporta, double x, double y)
+	{
+		int closestPixelX = (int) Math.round(x);
+		int closestPixelY = (int) Math.round(y);
+
+		return snaporta.getARGBAt(closestPixelX, closestPixelY);
+	}
+
+}
