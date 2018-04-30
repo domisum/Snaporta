38a9ba51b025ccd7d5688672e243a2de8100a9eb
Dominik <dominik.weissenseel@gmx.de>
2018-04-30 14:42:59 +0200

    Added gray color

--- src/main/java/de/domisum/lib/snaporta/color/Colors.java
+++ src/main/java/de/domisum/lib/snaporta/color/Colors.java
@@ -10,6 +10,7 @@ public final class Colors
 
 	@API public static final Color TRANSPARENT = Color.fromARGB(0, 0, 0, 0);
 	@API public static final Color BLACK = Color.fromBrightnessAbs(0);
+	@API public static final Color GRAY = Color.fromBrightnessAbs(255/2);
 	@API public static final Color WHITE = Color.fromBrightnessAbs(255);
 
 	@API public static final Color RED = Color.fromRGB(255, 0, 0);
