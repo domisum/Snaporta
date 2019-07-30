892d655fa70991a31b9a4f600f455390cee45118
domisum <dominik.weissenseel@gmail.com>
2019-07-30 19:25:35 +0200

    Added #containsTransparency() to Snaporta

--- src/main/java/de/domisum/lib/snaporta/Snaporta.java
+++ src/main/java/de/domisum/lib/snaporta/Snaporta.java
@@ -11,33 +11,51 @@ import de.domisum.lib.snaporta.util.Sized;
 public interface Snaporta extends Sized
 {
 
-	@API int getARGBAt(int x, int y);
+	@API
+	int getARGBAt(int x, int y);
 
-	@API default Color getColorAt(int x, int y)
+	@API
+	default Color getColorAt(int x, int y)
 	{
 		int argb = getARGBAt(x, y);
 		return Color.fromARGBInt(argb);
 	}
 
 
-	@API default int getAlphaAt(int x, int y)
+	@API
+	default int getAlphaAt(int x, int y)
 	{
 		return ARGBUtil.getAlphaComponent(getARGBAt(x, y));
 	}
 
-	@API default int getRedAt(int x, int y)
+	@API
+	default int getRedAt(int x, int y)
 	{
 		return ARGBUtil.getRedComponent(getARGBAt(x, y));
 	}
 
-	@API default int getGreenAt(int x, int y)
+	@API
+	default int getGreenAt(int x, int y)
 	{
 		return ARGBUtil.getGreenComponent(getARGBAt(x, y));
 	}
 
-	@API default int getBlueAt(int x, int y)
+	@API
+	default int getBlueAt(int x, int y)
 	{
 		return ARGBUtil.getBlueComponent(getARGBAt(x, y));
+	}
+
+
+	@API
+	default boolean containsTransparency()
+	{
+		for(int y = 0; y < getHeight(); y++)
+			for(int x = 0; x < getWidth(); x++)
+				if(getAlphaAt(x, y) > 0)
+					return true;
+
+		return false;
 	}
 
 }
