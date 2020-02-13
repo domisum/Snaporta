a82463fb26cde9af64252f3d651b5cd4b4237c4d
domisum <dominik.weissenseel@gmail.com>
2020-02-13 16:38:03 +0100

    Fixed off by one error in CroppedSnaporta

--- src/main/java/de/domisum/lib/snaporta/snaportas/transform/resize/CroppedSnaporta.java
+++ src/main/java/de/domisum/lib/snaporta/snaportas/transform/resize/CroppedSnaporta.java
@@ -26,8 +26,8 @@ public class CroppedSnaporta implements Snaporta
 	{
 		Validate.isTrue(width >= 1, "width has to be at least 1, was "+width);
 		Validate.isTrue(height >= 1, "height has to be at least 1, was "+height);
-		SnaportaValidate.validateInInterval(0, backingSnaporta.getWidth()-1, "cropX+width", cropX+width);
-		SnaportaValidate.validateInInterval(0, backingSnaporta.getHeight()-1, "cropY+height", cropY+height);
+		SnaportaValidate.validateInInterval(0, backingSnaporta.getWidth(), "cropX+width", cropX+width);
+		SnaportaValidate.validateInInterval(0, backingSnaporta.getHeight(), "cropY+height", cropY+height);
 
 		this.backingSnaporta = backingSnaporta;
 		this.cropX = cropX;
