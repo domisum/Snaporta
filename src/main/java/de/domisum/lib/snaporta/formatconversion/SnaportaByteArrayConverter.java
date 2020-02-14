99c6ecf3af16b03e3a37346e999e28ca2d46eea4
domisum <dominik.weissenseel@gmail.com>
2020-02-14 21:10:15 +0100

    Added SnaportaByteArrayConverter

---
+++ src/main/java/de/domisum/lib/snaporta/formatconversion/SnaportaByteArrayConverter.java
@@ -0,0 +1,34 @@
+package de.domisum.lib.snaporta.formatconversion;
+
+import de.domisum.lib.auxilium.util.java.annotations.API;
+import de.domisum.lib.snaporta.Snaporta;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import javax.imageio.ImageIO;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+
+@API
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class SnaportaByteArrayConverter
+{
+
+	public static byte[] convert(Snaporta snaporta)
+	{
+		var bufferedImage = SnaportaBufferedImageConverter.convert(snaporta);
+
+		try
+		{
+			var byteArrayOutputStream = new ByteArrayOutputStream();
+			ImageIO.write(bufferedImage, "png", byteArrayOutputStream);
+			return byteArrayOutputStream.toByteArray();
+		}
+		catch(IOException e)
+		{
+			throw new UncheckedIOException(e);
+		}
+	}
+
+}
