6d4858b0e0579b0c007c446a094d4e145654b998
domisum <dominik.weissenseel@gmail.com>
2020-02-19 15:48:42 +0100

    Use constants instead of literals

--- src/main/java/de/domisum/lib/snaporta/color/Colors.java
+++ src/main/java/de/domisum/lib/snaporta/color/Colors.java
@@ -10,18 +10,19 @@ public final class Colors
 
 	@API
 	public static final Color TRANSPARENT = Color.fromARGB(0, 0, 0, 0);
+
 	@API
 	public static final Color BLACK = Color.fromBrightnessAbs(0);
 	@API
-	public static final Color GRAY = Color.fromBrightnessAbs(255/2);
+	public static final Color GRAY = Color.fromBrightnessAbs(Color.COLOR_COMPONENT_MAX/2);
 	@API
-	public static final Color WHITE = Color.fromBrightnessAbs(255);
+	public static final Color WHITE = Color.fromBrightnessAbs(Color.COLOR_COMPONENT_MAX);
 
 	@API
-	public static final Color RED = Color.fromRGB(255, 0, 0);
+	public static final Color RED = Color.fromRGB(Color.COLOR_COMPONENT_MAX, 0, 0);
 	@API
-	public static final Color GREEN = Color.fromRGB(0, 255, 0);
+	public static final Color GREEN = Color.fromRGB(0, Color.COLOR_COMPONENT_MAX, 0);
 	@API
-	public static final Color BLUE = Color.fromRGB(0, 0, 255);
+	public static final Color BLUE = Color.fromRGB(0, 0, Color.COLOR_COMPONENT_MAX);
 
 }
