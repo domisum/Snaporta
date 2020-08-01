b4f68cd9f7c1916eea17dd03a1c2439ada43aa45
domisum <me@domisum.io>
2020-08-01 11:52:35 +0200

    Added BlankSnaporta

---
+++ src/main/java/io/domisum/lib/snaporta/snaportas/color/BlankSnaporta.java
@@ -0,0 +1,18 @@
+package io.domisum.lib.snaporta.snaportas.color;
+
+import io.domisum.lib.auxiliumlib.annotations.API;
+import io.domisum.lib.snaporta.color.Colors;
+
+@API
+public class BlankSnaporta
+		extends SolidColorSnaporta
+{
+	
+	// INIT
+	@API
+	public BlankSnaporta(int width, int height)
+	{
+		super(width, height, Colors.TRANSPARENT);
+	}
+	
+}
