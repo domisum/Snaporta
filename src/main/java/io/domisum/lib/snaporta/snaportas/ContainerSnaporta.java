868d715b8768aa8ed3774373f8406a58131c2714
domisum <me@domisum.io>
2020-10-30 12:30:03 +0100

    Add ContainerSnaporta

---
+++ src/main/java/io/domisum/lib/snaporta/snaportas/ContainerSnaporta.java
@@ -0,0 +1,43 @@
+package io.domisum.lib.snaporta.snaportas;
+
+import io.domisum.lib.auxiliumlib.annotations.API;
+import io.domisum.lib.auxiliumlib.util.ValidationUtil;
+import io.domisum.lib.snaporta.Snaporta;
+
+@API
+public class ContainerSnaporta
+	implements Snaporta
+{
+	
+	private final Snaporta contained;
+	
+	
+	// INIT
+	@API
+	public ContainerSnaporta(Snaporta contained)
+	{
+		ValidationUtil.notNull(contained, "contained");
+		this.contained = contained;
+	}
+	
+	
+	// SNAPORTA
+	@Override
+	public int getWidth()
+	{
+		return contained.getWidth();
+	}
+	
+	@Override
+	public int getHeight()
+	{
+		return contained.getHeight();
+	}
+	
+	@Override
+	public int getARGBAt(int x, int y)
+	{
+		return contained.getAlphaAt(x, y);
+	}
+	
+}
