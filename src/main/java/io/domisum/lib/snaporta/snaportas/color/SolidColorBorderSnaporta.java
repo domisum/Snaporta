13fa69d59d33a265467460167891d44241587860
domisum <me@domisum.io>
2020-11-15 19:03:29 +0100

    Simplify SolidColorBorderSnaporta

--- src/main/java/io/domisum/lib/snaporta/snaportas/color/SolidColorBorderSnaporta.java
+++ src/main/java/io/domisum/lib/snaporta/snaportas/color/SolidColorBorderSnaporta.java
@@ -1,61 +1,29 @@
 package io.domisum.lib.snaporta.snaportas.color;
 
 import io.domisum.lib.auxiliumlib.annotations.API;
-import io.domisum.lib.auxiliumlib.util.ValidationUtil;
 import io.domisum.lib.snaporta.Padding;
 import io.domisum.lib.snaporta.Snaporta;
 import io.domisum.lib.snaporta.color.Color;
-import io.domisum.lib.snaporta.util.SnaportaValidate;
+import io.domisum.lib.snaporta.snaportas.ContainerSnaporta;
+import io.domisum.lib.snaporta.snaportas.LayeredSnaporta;
+import io.domisum.lib.snaporta.snaportas.transform.ViewportSnaporta;
 
 @API
 public class SolidColorBorderSnaporta
-	implements Snaporta
+	extends ContainerSnaporta
 {
 	
-	private final Snaporta containedImage;
-	private final Padding borderPadding;
-	private final Color borderColor;
-	
-	
-	// INIT
 	@API
 	public SolidColorBorderSnaporta(Snaporta containedImage, Padding borderPadding, Color borderColor)
 	{
-		this.containedImage = containedImage;
-		this.borderPadding = borderPadding;
-		this.borderColor = borderColor;
-		
-		ValidationUtil.notNull(containedImage, "containedImage");
-		ValidationUtil.notNull(borderPadding, "borderPadding");
-		ValidationUtil.notNull(borderColor, "borderColor");
+		super(build(containedImage, borderPadding, borderColor));
 	}
 	
-	
-	// SNAPORTA
-	@Override
-	public int getWidth()
+	private static Snaporta build(Snaporta containedImage, Padding borderPadding, Color borderColor)
 	{
-		return containedImage.getWidth()+borderPadding.getHorizontalSum();
-	}
-	
-	@Override
-	public int getHeight()
-	{
-		return containedImage.getHeight()+borderPadding.getVerticalSum();
-	}
-	
-	@Override
-	public int getArgbAt(int x, int y)
-	{
-		SnaportaValidate.validateInBounds(this, x, y);
-		
-		int inContainedX = x-borderPadding.getLeft();
-		int inContainedY = y-borderPadding.getTop();
-		
-		if(containedImage.isInBounds(inContainedX, inContainedY))
-			return containedImage.getArgbAt(inContainedX, inContainedY);
-		else
-			return borderColor.toARGBInt();
+		var viewport = ViewportSnaporta.pad(containedImage, borderPadding);
+		var background = new SolidColorSnaporta(viewport.getWidth(), viewport.getHeight(), borderColor);
+		return new LayeredSnaporta(background, viewport);
 	}
 	
 }
