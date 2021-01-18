66df4166a641131b5ea1c6a0c672c34af13a74bc
domisum <me@domisum.io>
2021-01-18 18:34:31 +0100

    Add SnaportaRow

---
+++ src/main/java/io/domisum/lib/snaporta/snaportas/GeneratedSnaporta.java
@@ -0,0 +1,46 @@
+package io.domisum.lib.snaporta.snaportas;
+
+import com.google.common.base.Suppliers;
+import io.domisum.lib.auxiliumlib.annotations.API;
+import io.domisum.lib.snaporta.Snaporta;
+
+import java.util.function.Supplier;
+
+@API
+public abstract class GeneratedSnaporta
+	implements Snaporta
+{
+	
+	private final Supplier<Snaporta> generateOnce;
+	
+	
+	// INIT
+	@API
+	public GeneratedSnaporta()
+	{
+		generateOnce = Suppliers.memoize(this::generate);
+	}
+	
+	protected abstract Snaporta generate();
+	
+	
+	// SNAPORTA
+	@Override
+	public int getWidth()
+	{
+		return generateOnce.get().getWidth();
+	}
+	
+	@Override
+	public int getHeight()
+	{
+		return generateOnce.get().getHeight();
+	}
+	
+	@Override
+	public int getArgbAt(int x, int y)
+	{
+		return generateOnce.get().getArgbAt(x, y);
+	}
+	
+}
---
+++ src/main/java/io/domisum/lib/snaporta/snaportas/SnaportaRow.java
@@ -0,0 +1,130 @@
+package io.domisum.lib.snaporta.snaportas;
+
+import io.domisum.lib.auxiliumlib.PHR;
+import io.domisum.lib.auxiliumlib.annotations.API;
+import io.domisum.lib.auxiliumlib.exceptions.IncompleteCodeError;
+import io.domisum.lib.snaporta.Snaporta;
+import io.domisum.lib.snaporta.util.Sized;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@API
+@RequiredArgsConstructor
+public class SnaportaRow
+	extends GeneratedSnaporta
+{
+	
+	// CONSTANTS
+	private static final int DEFAULT_BETWEEN_PADDING = 0;
+	
+	// SETTINGS
+	private final List<Snaporta> snaportas;
+	private final RowDirection direction;
+	private final RowItemAlignment rowItemAlignment;
+	
+	private final int betweenPadding;
+	
+	
+	// INIT
+	@API
+	public static SnaportaRow horizontal(RowItemAlignment rowItemAlignment, int betweenPadding, List<Snaporta> snaportas)
+	{
+		return new SnaportaRow(snaportas, RowDirection.HORIZONTAL, rowItemAlignment, betweenPadding);
+	}
+	
+	@API
+	public static SnaportaRow horizontal(RowItemAlignment rowItemAlignment, List<Snaporta> snaportas)
+	{
+		return new SnaportaRow(snaportas, RowDirection.HORIZONTAL, rowItemAlignment, DEFAULT_BETWEEN_PADDING);
+	}
+	
+	@API
+	public static SnaportaRow vertical(RowItemAlignment rowItemAlignment, int betweenPadding, List<Snaporta> snaportas)
+	{
+		return new SnaportaRow(snaportas, RowDirection.VERTICAL, rowItemAlignment, betweenPadding);
+	}
+	
+	@API
+	public static SnaportaRow vertical(RowItemAlignment rowItemAlignment, List<Snaporta> snaportas)
+	{
+		return new SnaportaRow(snaportas, RowDirection.VERTICAL, rowItemAlignment, DEFAULT_BETWEEN_PADDING);
+	}
+	
+	
+	// GENERATE
+	@Override
+	protected Snaporta generate()
+	{
+		if(rowItemAlignment.getRowDirection() != null && rowItemAlignment.getRowDirection() != direction)
+			throw new IllegalArgumentException(PHR.r("Row direction {} doesn't support row item alignment {}",
+				direction, rowItemAlignment));
+		
+		int paddingSum = (snaportas.size()-1)*betweenPadding;
+		int width = direction == RowDirection.HORIZONTAL ?
+			snaportas.stream().mapToInt(Sized::getWidth).sum()+paddingSum :
+			snaportas.stream().mapToInt(Sized::getWidth).max().orElseThrow();
+		
+		int height = direction == RowDirection.HORIZONTAL ?
+			snaportas.stream().mapToInt(Sized::getHeight).max().orElseThrow() :
+			snaportas.stream().mapToInt(Sized::getHeight).sum()+paddingSum;
+		
+		var combined = new LayeredSnaporta(width, height);
+		int xOrY = 0;
+		for(var snaporta : snaportas)
+		{
+			int x = direction == RowDirection.HORIZONTAL ? xOrY : rowItemAlignment.calculateOffset(snaporta.getWidth(), width);
+			int y = direction == RowDirection.HORIZONTAL ? rowItemAlignment.calculateOffset(snaporta.getHeight(), height) : xOrY;
+			
+			combined.addLayerOnTop(snaporta, x, y);
+			
+			xOrY += direction == RowDirection.HORIZONTAL ? snaporta.getWidth() : snaporta.getHeight();
+			xOrY += betweenPadding;
+		}
+		
+		return combined;
+	}
+	
+	
+	// HELPER
+	public enum RowDirection
+	{
+		
+		HORIZONTAL,
+		VERTICAL
+		
+	}
+	
+	@RequiredArgsConstructor
+	public enum RowItemAlignment
+	{
+		
+		CENTER(null),
+		
+		TOP(RowDirection.HORIZONTAL),
+		BOTTOM(RowDirection.HORIZONTAL),
+		
+		LEFT(RowDirection.VERTICAL),
+		RIGHT(RowDirection.VERTICAL);
+		
+		
+		@Getter
+		private final RowDirection rowDirection;
+		
+		
+		public int calculateOffset(int itemSize, int spaceSize)
+		{
+			if(this == CENTER)
+				return (spaceSize-itemSize)/2;
+			else if(this == TOP || this == LEFT)
+				return 0;
+			else if(this == BOTTOM || this == RIGHT)
+				return spaceSize-itemSize;
+			else
+				throw new IncompleteCodeError();
+		}
+		
+	}
+	
+}
