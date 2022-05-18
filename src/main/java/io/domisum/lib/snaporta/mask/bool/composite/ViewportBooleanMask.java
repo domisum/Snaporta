c3679ba53dca6d3444f399af817ff4658070526b
domisum <me@domisum.io>
2022-05-18 14:58:17 +0200

    Add ViewportBooleanMask

---
+++ src/main/java/io/domisum/lib/snaporta/mask/bool/composite/ViewportBooleanMask.java
@@ -0,0 +1,60 @@
+package io.domisum.lib.snaporta.mask.bool.composite;
+
+import io.domisum.lib.auxiliumlib.annotations.API;
+import io.domisum.lib.snaporta.mask.bool.BooleanMask;
+import io.domisum.lib.snaporta.util.SnaportaValidate;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class ViewportBooleanMask
+	implements BooleanMask
+{
+	
+	private final BooleanMask baseMask;
+	private final int offsetX;
+	private final int offsetY;
+	private final int deltaWidth;
+	private final int deltaHeight;
+	
+	
+	// INIT
+	@API
+	public static ViewportBooleanMask offsetAndSizeDelta(BooleanMask baseMask, int offsetX, int offsetY, int deltaWidth, int deltaHeight)
+	{
+		return new ViewportBooleanMask(baseMask, offsetX, offsetY, deltaWidth, deltaHeight);
+	}
+	
+	public static ViewportBooleanMask extendLeftTopRightBottom(BooleanMask baseMask, int extendLeft, int extendTop, int extendRight, int extendBottom)
+	{
+		return new ViewportBooleanMask(baseMask, extendLeft, extendTop, extendLeft+extendRight, extendTop+extendBottom);
+	}
+	
+	
+	// MASK
+	@Override
+	public int getWidth()
+	{
+		return baseMask.getWidth()+deltaWidth;
+	}
+	
+	@Override
+	public int getHeight()
+	{
+		return baseMask.getHeight()+deltaHeight;
+	}
+	
+	@Override
+	public boolean getValueAt(int x, int y)
+	{
+		SnaportaValidate.validateInBounds(this, x, y);
+		
+		int bX = x-offsetX;
+		int bY = y-offsetY;
+		
+		if(baseMask.isOutOfBounds(bX, bY))
+			return false;
+		return baseMask.getValueAt(bX, bY);
+	}
+	
+}
