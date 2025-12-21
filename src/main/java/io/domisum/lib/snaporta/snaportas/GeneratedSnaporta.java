b07b05c1919bb03be9c132d0453c55ea2d79af63
domisum <me@domisum.io>
2025-12-21 11:16:21 +0100

    Cleanup in GeneratedSnaporta

--- src/main/java/io/domisum/lib/snaporta/snaportas/GeneratedSnaporta.java
+++ src/main/java/io/domisum/lib/snaporta/snaportas/GeneratedSnaporta.java
@@ -16,37 +16,26 @@ public abstract class GeneratedSnaporta
 	
 	// INIT
 	@API
-	public GeneratedSnaporta()
-	{
-		generateOnce = Suppliers.memoize(this::generate);
-	}
+	public GeneratedSnaporta() {generateOnce = Suppliers.memoize(this::generate);}
 	
 	protected abstract Snaporta generate();
 	
 	
 	// SNAPORTA
 	@Override
-	public int getWidth()
-	{
-		return generateOnce.get().getWidth();
-	}
+	public int getWidth() {return get().getWidth();}
 	
 	@Override
-	public int getHeight()
-	{
-		return generateOnce.get().getHeight();
-	}
+	public int getHeight() {return get().getHeight();}
 	
 	@Override
-	public int getArgbAt(int x, int y)
-	{
-		return generateOnce.get().getArgbAt(x, y);
-	}
+	public int getArgbAt(int x, int y) {return get().getArgbAt(x, y);}
 	
 	@Override
-	public BlankState isBlank()
-	{
-		return BlankState.UNKNOWN;
-	}
+	public BlankState isBlank() {return BlankState.UNKNOWN;}
+	
+	
+	// UTIL
+	private Snaporta get() {return generateOnce.get();}
 	
 }
